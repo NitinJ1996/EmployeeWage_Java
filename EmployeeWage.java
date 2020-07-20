d082b6940f3344039a6fa51641bf275652a65006
NitinJ1996 <nitin1996j@gmail.com>
2020-07-20 15:03:36 +0530

    {Nitin} - Employee Check using Random Created

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,1 +1,17 @@
-Welcome to EmployeeWage Computation Program
+//Welcome to EmployeeWage Computation Program
+
+import java.util.*;
+
+public class EmployeeWage{
+        public static void main(String args[])
+        {
+                Random random = new Random();
+                int employee = random.nextInt(2);
+                int ispresent=1;
+                if(employee==ispresent)
+                        System.out.println("Employee Present");
+                else
+                        System.out.println("Employee Absent");
+        }
+}
+
