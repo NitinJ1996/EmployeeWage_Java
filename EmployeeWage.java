07bd4d9d68c1ab286c754f983a092aa4d5a7db7c
NitinJ1996 <nitin1996j@gmail.com>
2020-07-20 15:52:08 +0530

    {Nitin} - Employee wage for month calculation Update

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,10 +6,9 @@ public class EmployeeWage{
         public static void main(String args[])
         {
                 Random random = new Random();
-                int employee = random.nextInt(3);
-                int ispresent=1;
-		int isparttime=2;
 		int wagePerHour = 20, workHours;
+		int monthDays=20, count=0;
+		int totalWorkHours=0, empWage;
                 /*if(employee==ispresent)
 		{
 			workHours = 8;
@@ -25,21 +24,27 @@ public class EmployeeWage{
 			workHours = 0;
                         System.out.println("Employee Absent");
 		}*/
-		switch (employee)
+		while(count<=monthDays)
 		{
-		case 1: 
-			workHours = 8;
-                        System.out.println("Employee Works Full Time");
-			break;
-		case 2: 
-			workHours = 4;
-                        System.out.println("Employee Works PartTime");
-			break;
-		default: 
-			workHours = 0;
-                        System.out.println("Employee Absent");
+			int employee = random.nextInt(3);
+                	int ispresent=1;
+                	int isparttime=2;
+			switch (employee)
+			{
+			case 1: 
+				workHours = 8;
+				break;
+			case 2: 
+				workHours = 4;
+				break;
+			default: 
+				workHours = 0;
+				break;
+			}
+			count++;
+			totalWorkHours+=workHours;
 		}
-		System.out.println("Employee Wage: "+ wagePerHour * workHours);
+		empWage=wagePerHour * totalWorkHours;
+		System.out.printf("Employee Wage for %d days: %d ", count-1, empWage);
 	}
 }
-
