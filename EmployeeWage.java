250624915e59ed880386046c80b600d5104fbac2
NitinJ1996 <nitin1996j@gmail.com>
2020-07-23 11:22:49 +0530

    {Nitin} Code Refactoring Update

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,36 +1,31 @@
 //Welcome to EmployeeWage Computation Program
-
 import java.util.*;
 public class EmployeeWage{
 
-	private int wagePerHour = 20, workHours;
-    	private int monthDays=20, count=0;
-    	private int totalWorkHours=0, totalWage;
+	private final int WAGEPERHOUR = 20, MONTHDAYS=20;
+    	private int totalWorkHours=0, workHours, totalWage, count=0; //instance variables
 	public void EmpWageCal() {
 		Random random = new Random();
-		while(count <= monthDays && totalWorkHours < 100)
+		while(this.count <= this.MONTHDAYS && this.totalWorkHours < 100)
 		{
-                    int employee = random.nextInt(3);
+                    int employee = random.nextInt(3); //local variable
                     switch (employee)
                     {
-                    case 1:
-                            workHours = 8;    break;
-                    case 2:
-                            workHours = 4;    break;
-                    default:
-                            workHours = 0;    break;
+                    case 1:  workHours = 8;   break;
+                    case 2:  workHours = 4;   break;
+                    default: workHours = 0;   break;
                     }
-                    count++;
+                    this.count++;
                     this.totalWorkHours+=this.workHours;
 		}
-            	this.totalWage=this.wagePerHour * this.totalWorkHours;
+            	this.totalWage=this.WAGEPERHOUR * this.totalWorkHours;
 	}
-	@Override
+	@Override //Overriding the inbuilt toString method
 	public String toString() {
 		if(count==21)
-			return String.format("Employee Wage for %d days and %d totalhours : %d ",count-1, totalWorkHours,  totalWage);
+			return String.format("Employee Wage for %d days and %d totalhours : %d ",this.count-1, this.totalWorkHours,  this.totalWage);
 		else
-			return String.format("Employee Wage for %d days and %d totalhours : %d ",count, totalWorkHours,  totalWage);
+			return String.format("Employee Wage for %d days and %d totalhours : %d ",this.count, this.totalWorkHours,  this.totalWage);
 	}
         public static void main(String args[])
         {
