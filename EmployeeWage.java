94a413d0e97858d143ddeeb233e7492a6c32731f
NitinJ1996 <nitin1996j@gmail.com>
2020-07-23 10:56:34 +0530

    {Nitin} - Code Refactoring using Class methods update

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,50 +1,41 @@
 //Welcome to EmployeeWage Computation Program
 
 import java.util.*;
-
 public class EmployeeWage{
-        public static void main(String args[])
-        {
-                Random random = new Random();
-		int wagePerHour = 20, workHours;
-		int monthDays=20, count=0;
-		int totalWorkHours=0, empWage;
-                /*if(employee==ispresent)
-		{
-			workHours = 8;
-                        System.out.println("Employee Works Full Time");
-                }
-		else if (employee==isparttime)
-		{
-			workHours = 4;
-			System.out.println("Employee Works PartTime");
-		}
-		else
-		{
-			workHours = 0;
-                        System.out.println("Employee Absent");
-		}*/
+
+	private int wagePerHour = 20, workHours;
+    	private int monthDays=20, count=0;
+    	private int totalWorkHours=0, totalWage;
+	public void EmpWageCal() {
+		Random random = new Random();
 		while(count <= monthDays && totalWorkHours < 100)
 		{
-			int employee = random.nextInt(3);
-                	int ispresent=1;
-                	int isparttime=2;
-			switch (employee)
-			{
-			case 1: 
-				workHours = 8;
-				break;
-			case 2: 
-				workHours = 4;
-				break;
-			default: 
-				workHours = 0;
-				break;
-			}
-			count++;
-			totalWorkHours+=workHours;
+                    int employee = random.nextInt(3);
+                    switch (employee)
+                    {
+                    case 1:
+                            workHours = 8;    break;
+                    case 2:
+                            workHours = 4;    break;
+                    default:
+                            workHours = 0;    break;
+                    }
+                    count++;
+                    this.totalWorkHours+=this.workHours;
 		}
-		empWage=wagePerHour * totalWorkHours;
-		System.out.printf("Employee Wage for %d days and %d totalhours : %d ", count-1,totalWorkHours, empWage);
+            	this.totalWage=this.wagePerHour * this.totalWorkHours;
 	}
+	@Override
+	public String toString() {
+		if(count==21)
+			return String.format("Employee Wage for %d days and %d totalhours : %d ",count-1, totalWorkHours,  totalWage);
+		else
+			return String.format("Employee Wage for %d days and %d totalhours : %d ",count, totalWorkHours,  totalWage);
+	}
+        public static void main(String args[])
+        {
+                EmployeeWage object = new EmployeeWage();
+                object.EmpWageCal();
+                System.out.println(object);
+        }
 }
