d2a4ce40b266f20937335190472f147de647ec25
NitinJ1996 <nitin1996j@gmail.com>
2020-07-24 11:21:06 +0530

    {Nitin} Wages for multiple companies Update

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,12 +2,15 @@
 import java.util.*;
 public class EmployeeWage{
 
-	private final int WAGEPERHOUR = 20, MONTHDAYS=20;
-    	private int totalWorkHours=0, workHours, totalWage, count=0; //instance variables
-	public void EmpWageCal() {
-		Random random = new Random();
-		while(this.count <= this.MONTHDAYS && this.totalWorkHours < 100)
-		{
+        //private final int WAGEPERHOUR = 20, MONTHDAYS=20; //instance variable final
+	private String cName; //Instance variables
+
+        public void EmpWageCal(int wagePerHour, int maxWorkingDays, int maxWorkingHours, String companyName)
+        {
+		int totalWorkHours=0, workHours, totalWage, count=0;
+                Random random = new Random();
+                while(count <= maxWorkingDays && totalWorkHours < maxWorkingHours)
+                {
                     int employee = random.nextInt(3); //local variable
                     switch (employee)
                     {
@@ -15,22 +18,37 @@ public class EmployeeWage{
                     case 2:  workHours = 4;   break;
                     default: workHours = 0;   break;
                     }
-                    this.count++;
-                    this.totalWorkHours+=this.workHours;
-		}
-            	this.totalWage=this.WAGEPERHOUR * this.totalWorkHours;
-	}
-	@Override //Overriding the inbuilt toString method
-	public String toString() {
-		if(count==21)
-			return String.format("Employee Wage for %d days and %d totalhours : %d ",this.count-1, this.totalWorkHours,  this.totalWage);
-		else
-			return String.format("Employee Wage for %d days and %d totalhours : %d ",this.count, this.totalWorkHours,  this.totalWage);
-	}
-        public static void main(String args[])
-        {
-                EmployeeWage object = new EmployeeWage();
-                object.EmpWageCal();
-                System.out.println(object);
+                    count++;
+                    totalWorkHours+=workHours;
+                }
+                totalWage=wagePerHour * totalWorkHours;
+		toString(companyName,count,totalWorkHours,totalWage);
         }
+
+       // @Override //Overriding the inbuilt toString method
+        public static String toString(String companyName, int count, int totalWorkHours, int totalWage) {
+                if(count==21)
+                        return String.format("Company %s: Employee Wage for %d days and %d totalhours : %d ", companyName, count-1, totalWorkHours,  totalWage);
+                else
+                        return String.format("Company %s: Employee Wage for %d days and %d totalhours : %d ", companyName, count, totalWorkHours,  totalWage);
+        }
+
+	public static void main(String args[]){
+		Scanner input =  new Scanner(System.in);
+                System.out.print("Enter the number of Companies: \t");
+                int companyCount = input.nextInt();
+                for(int i = 0; i<companyCount; i++)
+                {
+                        System.out.println("Enter the Name of the Company:");
+                        String companyName = input.next();
+                        System.out.println("Enter the number of Max Working Days in a month:");
+                        int maxWorkingDays = input.nextInt();
+                        System.out.println("Enter the number of Max Working Hours in a month:");
+                        int maxWorkingHours = input.nextInt();
+                        System.out.println("Enter the Wage per hour of Company:");
+                        int wagePerHour = input.nextInt();
+                        EmployeeWage object = new EmployeeWage();
+                        object.EmpWageCal(wagePerHour,maxWorkingDays,maxWorkingHours,companyName);
+		}
+	}
 }
