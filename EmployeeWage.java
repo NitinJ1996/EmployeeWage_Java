5326458b423e8a1af740398aeea119a5a49181f8
NitinJ1996 <nitin1996j@gmail.com>
2020-07-24 11:42:10 +0530

    {Nitin} Code Refactoring, remove instance variables Update

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class EmployeeWage{
 
         //private final int WAGEPERHOUR = 20, MONTHDAYS=20; //instance variable final
-	private String cName; //Instance variables
-
-        public void EmpWageCal(int wagePerHour, int maxWorkingDays, int maxWorkingHours, String companyName)
+        public static void EmpWageCal(int wagePerHour, int maxWorkingDays, int maxWorkingHours, String companyName)
         {
 		int totalWorkHours=0, workHours, totalWage, count=0;
                 Random random = new Random();
@@ -22,12 +20,12 @@ public class EmployeeWage{
                     totalWorkHours+=workHours;
                 }
                 totalWage=wagePerHour * totalWorkHours;
-		toString(companyName,count,totalWorkHours,totalWage);
+		System.out.println(toPrint(companyName,count,totalWorkHours,totalWage,maxWorkingDays));
         }
 
        // @Override //Overriding the inbuilt toString method
-        public static String toString(String companyName, int count, int totalWorkHours, int totalWage) {
-                if(count==21)
+        public static String toPrint(String companyName, int count, int totalWorkHours, int totalWage, int maxWorkingDays) {
+                if(count==maxWorkingDays)
                         return String.format("Company %s: Employee Wage for %d days and %d totalhours : %d ", companyName, count-1, totalWorkHours,  totalWage);
                 else
                         return String.format("Company %s: Employee Wage for %d days and %d totalhours : %d ", companyName, count, totalWorkHours,  totalWage);
@@ -39,16 +37,15 @@ public class EmployeeWage{
                 int companyCount = input.nextInt();
                 for(int i = 0; i<companyCount; i++)
                 {
-                        System.out.println("Enter the Name of the Company:");
+                        System.out.print("Enter the Name of the Company: ");
                         String companyName = input.next();
-                        System.out.println("Enter the number of Max Working Days in a month:");
+                        System.out.print("Enter the number of Max Working Days in a month: ");
                         int maxWorkingDays = input.nextInt();
-                        System.out.println("Enter the number of Max Working Hours in a month:");
+                        System.out.print("Enter the number of Max Working Hours in a month: ");
                         int maxWorkingHours = input.nextInt();
-                        System.out.println("Enter the Wage per hour of Company:");
+                        System.out.print("Enter the Wage per hour of Company: ");
                         int wagePerHour = input.nextInt();
-                        EmployeeWage object = new EmployeeWage();
-                        object.EmpWageCal(wagePerHour,maxWorkingDays,maxWorkingHours,companyName);
+                        EmpWageCal(wagePerHour,maxWorkingDays,maxWorkingHours,companyName);
 		}
 	}
 }
